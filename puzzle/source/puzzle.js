872777eb186e45e57ec0dc1815a87f8c3065281f
stevenfranklin2808 <stevenfranklin2808@yahoo.com>
2013-10-20 21:04:54 -0500

    MEETSAM: add drag and drop functions

--- puzzle/source/puzzle.js
+++ puzzle/source/puzzle.js
@@ -24,6 +24,7 @@ function initImage(puzzleNum){
         newImg.src = "./images/img"+puzzleNum+"-"+count[picNum]+".jpg";
         newImg.setAttribute("class", "img");
         newImg.setAttribute("id", "img"+count[picNum]);
+        newImg.setAttribute("onmousedown", "grabber(event);");
         if(temp2 > 3)
             {
             temp1 = temp1 + 1;
@@ -48,6 +49,77 @@ function Timer() {
         document.getElementById("timeTaken").value = str;
     },1000);
 }
+
+/*
+ Define variables for the values computed by the grabber event 
+ handler but needed by mover event handler
+*/
+var diffX, diffY, theElement;
+
+
+// The event handler function for grabbing the word
+function grabber(event) {
+
+// Set the global variable for the element to be moved
+
+  theElement = event.currentTarget;
+
+// Determine the position of the word to be grabbed,
+//  first removing the units from left and top
+
+  var posX = parseInt(theElement.style.left);
+  var posY = parseInt(theElement.style.top);
+
+// Compute the difference between where it is and
+// where the mouse click occurred
+
+  diffX = event.clientX - posX;
+  diffY = event.clientY - posY;
+
+// Now register the event handlers for moving and
+// dropping the word
+
+  document.addEventListener("mousemove", mover, true);
+  document.addEventListener("mouseup", dropper, true);
+
+// Stop propagation of the event and stop any default
+// browser action
+
+  event.stopPropagation();
+  event.preventDefault();
+
+}  //** end of grabber
+
+// *******************************************************
+
+// The event handler function for moving the word
+
+function mover(event) {
+// Compute the new position, add the units, and move the word
+
+  theElement.style.left = (event.clientX - diffX) + "px";
+  theElement.style.top = (event.clientY - diffY) + "px";
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of mover
+
+// *********************************************************
+// The event handler function for dropping the word
+
+function dropper(event) {
+
+// Unregister the event handlers for mouseup and mousemove
+
+  document.removeEventListener("mouseup", dropper, true);
+  document.removeEventListener("mousemove", mover, true);
+
+// Prevent propagation of the event
+
+  event.stopPropagation();
+}  //** end of dropper
+
 /*
 //Function will check individual tile placement.
 function checkMatch(sqrNumber, tileNumber)
