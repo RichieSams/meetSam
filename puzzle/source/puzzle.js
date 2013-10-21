a75c61bf00b6d4189949842f118011b294610bbe
Martin Valenzuela <mars110110@aol.com>
2013-10-21 01:07:04 -0500

    Add timer variable.

--- puzzle/source/puzzle.js
+++ puzzle/source/puzzle.js
@@ -2,7 +2,7 @@
  Define variables for the values computed by the grabber event
  handler but needed by mover event handler
  */
-var diffX, diffY, theElement, origX, origY;
+var time, timer, diffX, diffY, theElement, origX, origY;
 var tileArray = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
 
 //Function initiates the random images right of the grid.
