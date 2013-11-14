5309f9d56858a6da8ebd6a6a4f91ebecc35ed379
Adrian Astley <adastley@gmail.com>
2013-11-14 15:51:42 -0600

    QUIZ: More refactoring and spliting more user data into thier own files
    The last thing that needs to be done is actually add the questions and
    grading. IE. implement getQuestion() and gradeQuestion()

--- quiz/index.php
+++ quiz/index.php
@@ -1,4 +1,7 @@
-<?php session_start(); ?>
+<?php
+// TODO: Add logic to store the session ID in a cookie just in case the user's browser crashes or they accidentally close the window
+session_start();
+?>
 
 <html>
 <head>
@@ -9,102 +12,117 @@
 
 <?php
 
+// TODO: Perhaps split some of this logic into separate php files?
+
 if(isset($_POST["loggedIn"])){
-    startQuiz();
+    if (!checkLogin()) {
+        echo "<h1>Incorrect Username or Password</h1>";
+        createLoginForm();
+        return;
+    }
+
+    if (isset($_SESSION["questionNumber"])) {
+        // Check if user has time left
+        if (time() - $_SESSION["startTime"] > 900) {
+            echo "<h3>You are out of time</h3>";
+
+            // Write results to file
+            $file = fopen("results.txt", "a");
+            fwrite($file, $_SESSION["userName"] . ":" . $_SESSION['results'] . "\n");
+            fclose($file);
+            return;
+        }
+
+        gradeQuestion($_SESSION["questionNumber"]);
+        getQuestion(++$_SESSION["questionNumber"]);
+    } else {
+        startQuiz();
+    }
 } else {
-    logIn();
+    createLoginForm();
 }
 
 
-#################################################################
-function logIn(){
+function createLoginForm(){
     echo '<h1>Log In</h1>
-          <form  id="login" action="' . $_SERVER['PHP_SELF'] . '" method="POST"
-          onsubmit="return validate();">
-
-          <div class = "userName">
-              <label> Name:
-                  <input type="text" name="name" maxlength = "30"/>
-              </label>
-          </div>
-          <div class = "password">
-              <label> Password:
-                <input type = "password" name = "passwd" value = "" maxlength = "30"/>
-              </label><br/><br/>
-          </div>
-          <input type = "submit" value = "Submit" name = "loggedIn"/>
-          <input type = "reset" value = "Reset" />
+          <form action="' . $_SERVER['PHP_SELF'] . '" method="POST" onsubmit="return validate();">
+              <div class="userName">
+                  <label> Name:
+                      <input type="text" name="name" maxlength="30" />
+                  </label>
+              </div>
+              <div class="password">
+                  <label> Password:
+                      <input type="password" name="pass" maxlength="30" />
+                  </label>
+                  <br/><br/>
+              </div>
+              <input type="submit" value="Submit" name="loggedIn" />
+              <input type="reset" value="Reset" />
           </form>';
 }
 
 
-//checks for login information and initiates quiz
-function startQuiz() {
-    $check = false;
-    $passwd = $_POST["passwd"];
+function checkLogin() {
     $name = $_POST["name"];
+    $pass = $_POST["pass"];
 
-    $hasStarted = 0;
-
-    $fileContents = "";
     $file = fopen("passwd.txt", "r");
     while(!feof($file)) {
-        $line = fgets($file);
+        $line = trim(fgets($file));
         $lineElements = explode(':', $line);
 
-        if ($name == $lineElements[0] && $passwd == $lineElements[1]) {
-            $hasStarted = trim($lineElements[2]);
-            $check = true;
-        } else {
-            $fileContents .= $line;
+        if ($name == $lineElements[0] && $pass == $lineElements[1]) {
+            fclose($file);
+            $_SESSION["userName"] = $name;
+            return true;
+        }
+    }
+
+    fclose($file);
+    return false;
+}
+
+
+function startQuiz() {
+    // Find out if the user has already started the test
+    $file = fopen("started.txt", "r");
+    while(!feof($file)) {
+        $line = trim(fgets($file));
+        $lineElements = explode(':', $line);
+
+        if ($_SESSION["userName"] == $lineElements[0]) {
+            echo "<h3>You have already taken the test</h3>";
+            fclose($file);
+            return;
         }
     }
     fclose($file);
 
-    if (!$check) {
-        echo "<h1>Incorrect Username or Password</h1>";
-        logIn();
-    } else {
-        if ($hasStarted == "0") {
-            // Check if the user has any time left
-            if (!isset($_SESSION["results"])) {
+    // Start the user
+    fopen("started.txt", "a");
+    fwrite($file, $_SESSION["userName"] . "\n");
+    fclose($file);
 
-            }
-            if (!isset($_SESSION["startTime"])) {
-                $_SESSION["startTime"] = time();
-            } elseif (time() - $_SESSION["startTime"] > 900) {
-                echo "<h3>You are out of time</h3>";
+    $_SESSION["results"] = 0;
+    $_SESSION["startTime"] = time();
+    $_SESSION["questionNumber"] = 1;
 
-                return;
-            }
-
-            if (!isset($_SESSION["questionNumber"])) {
-                $_SESSION["questionNumber"] = 1;
-            }
-
-            // Update the passwd file to signal that the user has started the test
-            $file = fopen("passwd.txt", "w");
-            fwrite($file, $fileContents . "$name:$passwd:1");
-            fclose($file);
-
-            changeStart($name);
-            echo '<p>Thank you for logging in for your quiz. You will have six question on basic Astronomy that you must answer in 15 min. You may only take the quiz once. Good luck!</p>';
-            quiz();
-        } else {
-            echo "<h3>You have already taken the test</h3>";
-        }
-    }
+    echo '<p>Thank you for logging in for your quiz. You will have six question on basic Astronomy that you must answer in 15 min. You may only take the quiz once. Good luck!</p>';
+    getQuestion($_SESSION["questionNumber"]);
 }
 
+function gradeQuestion($questionNumber) {
+
+}
 
 function getQuestion($questionNumber){
+    $question = array( '<form></form>',
+                       '<form></form>');
 
-}
 
-#################################################################
-// will out put and grade questions
-function quiz(){
 
+    return $question[$questionNumber];
 }
 
 ?>
--- quiz/validate.js
+++ quiz/validate.js
@@ -1,16 +1,14 @@
-document.getElementById("logInForm").onsubmit = validate;
-  function validate ()
-  {
-    var elt = document.getElementById("logInForm");
-    if (elt.name.value == "")
-    {
-      window.alert ("Enter name");
-      return false;
+function validate () {
+    var form = document.getElementById("logInForm");
+    
+    if (form.name.value == "") {
+        window.alert ("Enter name");
+        return false;
     }
-    if (elt.comments.value == "")
-    {
-      window.alert ("Enter Password");
-      return false;
+    if (form.comments.value == "") {
+        window.alert ("Enter Password");
+        return false;
     }
+
     return true;
-  }
+}
