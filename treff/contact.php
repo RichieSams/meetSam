c1458e9b2556352923b91b4d89c2254eace56c10
Adrian Astley <adastley@gmail.com>
2013-11-18 17:48:10 -0600

    TREFF: Move all header and footer html to php files and then include them in each new file
    This makes it *much* easier to edit the header and footer.

---
+++ treff/contact.php
@@ -0,0 +1,12 @@
+<?php include 'header.php'; ?>
+
+<div class="main_body">
+	<div class="contact">
+		<h1> Meet SAM </h1>
+		<p>Steven<br/>A Mechanical Engineering major at the University of Texas at Austin</p>
+		<p>Adrian<br/>A Mechanical Engineering major at the University of Texas at Austin<br/>@adastley</p>
+		<p>Martin<br/>A Physics major at the University of Texas at Austin<br/>@Lothilius</p>
+	</div>
+</div> <!-- End of main_body -->
+
+<?php include 'footer.php'; ?>
---
+++ treff/index.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+
+<div class="main_body">
+	<div class="info">
+		<h1>Need a half-way meeting point?</h1>
+		<p>
+            Treff is a service for creating meeting points for people.<br /><br />
+            Treff will ask for your location and the phone numbers or emails of the people
+            you would like to meet up with. Then it will find a meeting point and automatically
+            send out personalized directions to everyone.
+	    </p>
+	</div>
+
+	<div class="login">
+		<div class="create">
+			<a href="dummy.php">Create a Treff</a>
+		</div>
+		<div class="join">
+			<a href="dummy.php">Join a Treff</a>
+		</div>
+	</div>
+</div> <!-- End of main_body -->
+
+<?php include 'footer.php'; ?>
