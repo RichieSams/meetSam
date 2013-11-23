32d150fffd70883bc2d5b10e9af3a7828602b731
Martin Valenzuela <mars110110@aol.com>
2013-11-23 13:36:00 -0600

    Change google call to true.

--- treff/googletest.php
+++ treff/googletest.php
@@ -12,7 +12,7 @@ body { height: 100%; margin: 0; padding: 0 }
 #map-canvas { height: 100% }
 </style>
 <script type="text/javascript"
-src="https://maps.googleapis.com/maps/api/js?key=<?php echo $goKey ?>&sensor=SET_TO_TRUE_OR_FALSE">
+src="https://maps.googleapis.com/maps/api/js?key=<?php echo $goKey ?>&sensor=true">
 </script>
 <script type="text/javascript">
 function initialize() {
