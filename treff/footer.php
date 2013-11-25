e52d1921eeb63942a4526f7304d4755c21ebbb4d
martin valenzuela <mars110110@aol.com>
2013-11-25 02:13:10 -0600

    Change last edit date. to nov 25th

--- treff/footer.php
+++ treff/footer.php
@@ -6,7 +6,7 @@ echo '
         <li></li>
         <li><a href="./contact.php">Contact Us</a></li>
         <li><div>&copy; Treff 2013</div></li>
-        <li><div>Last edited SAM Nov. 18, 2013</div></li>
+        <li><div>Last edited SAM Nov. 25, 2013</div></li>
 
     </ul>
 </div>
