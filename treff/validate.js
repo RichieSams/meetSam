187d629cddae50bc2a75a5d580940b5c4d7644c5
Steven Franklin <stevenfranklin2808@yahoo.com>
2013-11-25 18:34:32 -0600

    continued work on validate

--- treff/validate.js
+++ treff/validate.js
@@ -1,3 +1,5 @@
+var regExEmail = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
 document.getElementById("loginForm").onsubmit = validateLogin;
 function validateLogin () {
     var form = document.getElementById("loginForm");
@@ -14,7 +16,6 @@ function validateLogin () {
     return true;
 }
 
-var regExEmail = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 document.getElementById("registrationForm").onsubmit = validateRegistation;
 function validateLogin () {
     var form = document.getElementById("registrationForm");
@@ -22,14 +23,56 @@ function validateLogin () {
 	var passDig = /[0-9]/.test(form.pass1.value);
 	var passLen = /^[A-Za-z0-9_]{6,10}$/.test(form.pass1.value);
 
-    if (form.name.value == "" && ) {
-        window.alert ("Enter name");
-        return false;
-    }
-    if (form.pass.value == "") {
-        window.alert ("Enter Password");
+    if (form.name.value == "" || form.pass1.value == "" || form.street.value == "" || form.city.value == "" ) {
+        window.alert ("Incomplete Registration");
         return false;
     }
 
-    return true;
+	if (form.zip.value == "" || form.state.value == "") {
+        window.alert ("Incomplete Registration");
+        return false;
+    }
+
+    if (checkEmail != true) {
+        window.alert ("Invalid Email");
+        return false;
+    }
+
+	 if (passDig != true || passLen != true) {
+        window.alert ("Invalid Password");
+        return false;
+    }
+	
+	if (form.pass1.value != form.pass1.value){
+		window.alert ("Passwords do not match");
+		return false;
+	}
+
+	return true;
+}
+
+document.getElementById("treffForm").onsubmit = validateTreff;
+function validateTreff () {
+    var form = document.getElementById("treffForm");
+    var checkEmail1 = regExEmail.test(form.userName.value)
+	var checkEmail2 = regExEmail.test(form.treffMate.value)
+
+    if (form.userName.value == "" || form.treffMate.value == "" || form.street.value == "" || form.city.value == "" ) {
+        window.alert ("Incomplete Treff");
+        return false;
+    }
+
+	if (form.zip.value == "" || form.state.value == "") {
+        window.alert ("Incomplete Treff");
+        return false;
+    }
+
+    if (checkEmail1 != true || checkEmail1 != true) {
+        window.alert ("Invalid Email");
+        return false;
+    }
+
+	 
+
+	return true;
 }
