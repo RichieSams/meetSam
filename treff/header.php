5d550909f1dfe3256089853b195640816b7ef7d9
martin valenzuela <mars110110@aol.com>
2013-11-25 20:46:29 -0600

    Include jsfunctions file

--- treff/header.php
+++ treff/header.php
@@ -8,7 +8,7 @@ echo '
     <link rel="stylesheet" type="text/css" href="style.css" media="all" />
     <link rel="icon" href="favicon.ico" />
     <link rel="shortcut icon" href="http://example.com/favicon.ico" />
-    <script type ="text/javascript" src = "validate.js"></script>
+    <script type ="text/javascript" src = "jsfunctions.js"></script>
 </head>
 <body>
 <div class="header">
