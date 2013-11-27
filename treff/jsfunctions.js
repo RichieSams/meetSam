ec86686df3ee9c47924ed0ee788beeb14fc42fcc
martin valenzuela <mars110110@aol.com>
2013-11-27 00:23:55 -0600

    Add directions section to google api.

--- treff/jsfunctions.js
+++ treff/jsfunctions.js
@@ -1,27 +1,30 @@
+//-----Start Google Maps API ------>
 var map;
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 var geocoder = new google.maps.Geocoder();
 var centerDefault = '2315 Speedway, Austin, TX  78712-1528';
 
 //Get directions using google API
 function getDirects(address)
 {
-    alert("getting directions for "+address);
-    document.getElementById("directions").innerHTML="The directions to the Treff will be placed here.";
-    codeAddress(address);
+    alert("Getting directions for "+address);
+    document.getElementById("directions-panel").innerHTML="";
+    calcRoute(address,centerDefault);
 }
 
-//-----Start Google Maps API ------>
-
 function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
     center: new google.maps.LatLng(-34.397, 150.644),
     zoom: 17
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
                               mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
 
 //Goggle API for geocoding.
 function codeAddress(centerAdd) {
@@ -40,5 +43,20 @@ function codeAddress(centerAdd) {
                      }
                      });
 }
+
+function calcRoute(start,end) {
+    var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+    };
+    directionsService.route(request, function(response, status) {
+                            if (status == google.maps.DirectionsStatus.OK) {
+                            directionsDisplay.setDirections(response);
+                            }
+                            });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 //-----End Google Maps API ------>
