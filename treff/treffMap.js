e19f14b3a125897fae52154367dd60e0ff19e96c
Adrian Astley <adastley@gmail.com>
2013-12-08 17:50:40 -0600

    TREFF: Break jsfunctions into smaller, more personalized files.

---
+++ treff/treffMap.js
@@ -0,0 +1,40 @@
+var map;
+var directionsDisplay;
+var centerDefault = '2315 Speedway, Austin, TX  78712-1528';
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = {
+        center: new google.maps.LatLng(-34.397, 150.644),
+        zoom: 17
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('directions-panel'));
+}
+
+function setMapCenterFromAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            setMapCenterFromLocation(results[0].geometry.location);
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+function setMapCenterFromLocation(location) {
+    map.setCenter(location);
+}
+
+//Get directions using google API
+function getDirections(address)
+{
+    alert("Getting directions for "+address);
+    document.getElementById("directions-panel").innerHTML="";
+    calcRoute(address, centerDefault);
+}
+
