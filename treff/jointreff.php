89417f52d9712317f21c87b48b48da879fce7354
Adrian Astley <adastley@gmail.com>
2013-12-09 22:23:51 -0600

    TREFF: Finish jointreff.php

--- treff/jointreff.php
+++ treff/jointreff.php
@@ -3,14 +3,34 @@
 session_start();
 
 include_once 'functions.php';
+
+if (isset($_POST['idHash'])) {
+    $idHash = $_POST['idHash'];
+} elseif (isset($_GET['idHash'])) {
+    $idHash = $_GET['idHash'];
+} else {
+    errorPage();
+}
+
 createHeader(array("style.css"), array("validate.js"));
+
+$connection = connectMySql();
+
+$result = $connection->query("SELECT Meetings.name AS treffName
+                              FROM MeetingUsers
+                              INNER JOIN Meetings
+                              ON Meetings.meetingId = MeetingUsers.meetingId
+                              WHERE MeetingUsers.idHash='$idHash'");
+
+$treffName = $result->fetch_assoc()['treffName'];
+$result->free();
 
 echo '
 <div class="main_body">
     <div class="information">
-        <h1>Make a Treff</h1>
-        <form id="treffForm" value="'. $_SERVER['PHP_SELF'] . '" method="POST" onsubmit="return validateJoin();">
-            <input type="hidden" name="idHash" value="' . $_POST['idHash'] . '" />
+        <h1>Join "' . $treffName . '"</h1>
+        <form id="treffForm" value="process_treff.php" method="POST" onsubmit="return validateJoin();">
+            <input type="hidden" name="idHash" value="'. $idHash . '" />
             <table>
                 <tr>
                     <td><input type="text" name="email" maxlength="50" placeholder="Email Address"/></td>
